1efcfaa8b936364050254fb835490ae2b85ec123
luo-xi <luoxi.nus.ece@gmail.com>
2020-03-01 13:30:52 +0800

    Updates

---
+++ precache-manifest.47529d238fb501c10bfd77d1215c7f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adccdfce27928de50b49e4b11aad63a0",
+    "url": "/MSF_Dashboard/index.html"
+  },
+  {
+    "revision": "ff488e826a2bd8efd26d",
+    "url": "/MSF_Dashboard/static/css/main.8cb3d081.chunk.css"
+  },
+  {
+    "revision": "e426b99bf75447b638fe",
+    "url": "/MSF_Dashboard/static/js/2.f1aaca7b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MSF_Dashboard/static/js/2.f1aaca7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff488e826a2bd8efd26d",
+    "url": "/MSF_Dashboard/static/js/main.2ea354a5.chunk.js"
+  },
+  {
+    "revision": "375de2a71b228c08c11d",
+    "url": "/MSF_Dashboard/static/js/runtime-main.86d18517.js"
+  }
+]);
